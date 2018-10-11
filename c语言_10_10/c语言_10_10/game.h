dd418875d189b0bc7569496675acfed4576f0b59
coolnut12138 <1426345106@qq.com>
2018-10-11 21:50:51 +0800

    2018.10.11c语言代码上传

---
+++ c语言_10_10/c语言_10_10/game.h
@@ -0,0 +1,30 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+
+
+#define ROW 3
+#define COL 3
+
+
+void Init_board(char board[ROW][COL],int row,int col);
+void Display_board(char board[ROW][COL],int row,int col);
+void Player_move(char board[ROW][COL],int row,int col);
+void Computer_move(char board[ROW][COL],int row,int col);
+void game();
+int Is_full(char board[ROW][COL],int row,int col);
+
+
+//Is_win
+//'*'玩家赢
+//'o'电脑赢
+//'q'平局
+//' '再来
+char Is_win(char board[ROW][COL],int row,int col);
+
+
+#endif//__GAME_H__
